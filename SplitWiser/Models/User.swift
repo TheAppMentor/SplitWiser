151efdaddb8ec8aa61a8e959c7c094eade857bb7
Santebidanur Nagaraja, Pavan Kowshik <pavan.kowshik.santebidanur.nagaraja@sap.com>
2017-12-07 20:05:35 +0530

    Hopefully the first of many checkins :)

---
+++ SplitWiser/Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  SplitWiser
+//
+//  Created by Santebidanur Nagaraja, Pavan Kowshik on 12/7/17.
+//  Copyright © 2017 Santebidanur Nagaraja, Pavan Kowshik. All rights reserved.
+//
+
+import Foundation
+
+class User {
+	
+	
+}
