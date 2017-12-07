cca9fd82c0cf4c17b6d3e47207db14b1e4d4a7b4
Raghu, Reshma L <reshma.raghu@sap.com>
2017-12-07 21:01:09 +0530

    Skeleton of the Event fellow

---
+++ SplitWiser/Models/Event.swift
@@ -0,0 +1,19 @@
+//
+//  Event.swift
+//  SplitWiser
+//
+//  Created by Raghu, Reshma L on 12/7/17.
+//  Copyright © 2017 Santebidanur Nagaraja, Pavan Kowshik. All rights reserved.
+//
+
+import Foundation
+
+class Event {
+	var eventId: String!
+	var name: String!
+	var description: String!
+	var date: Date!
+	var amount: Int!
+	var paidBy: [User]!
+	var participants: [User]!
+}
