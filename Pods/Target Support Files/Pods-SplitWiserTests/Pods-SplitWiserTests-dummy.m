6863e615e38ae38044f8092f8ed2a216c5ad75e5
Reshma L Raghu <reshma.raghuvar@gmail.com>
2017-12-09 10:15:07 +0530

    Removing everything under *Pods* directory from version control

--- Pods/Target Support Files/Pods-SplitWiserTests/Pods-SplitWiserTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SplitWiserTests : NSObject
-@end
-@implementation PodsDummy_Pods_SplitWiserTests
-@end
--- Pods/Target Support Files/Pods-SplitWiserUITests/Pods-SplitWiserUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SplitWiserUITests : NSObject
-@end
-@implementation PodsDummy_Pods_SplitWiserUITests
-@end
