a01cd63d1844fe5788a0bac915da3f988e974a0d
Reshma L Raghu <reshma.raghuvar@gmail.com>
2017-12-10 14:06:38 +0530

    Error handling for Events

---
+++ SplitWiser/Error/EventError.swift
@@ -0,0 +1,13 @@
+//
+//  EventError.swift
+//  SplitWiser
+//
+//  Created by Raghu, Reshma L on 12/10/17.
+//  Copyright © 2017 Santebidanur Nagaraja, Pavan Kowshik. All rights reserved.
+//
+
+import Foundation
+
+enum EventError: Error {
+	case genericError
+}
