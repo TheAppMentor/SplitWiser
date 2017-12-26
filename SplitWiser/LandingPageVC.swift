a90b140b610a56ed6a12f7a395007ff1e42bb28e
Unknown <chapparfellow@gmail.com>
2017-12-26 22:23:30 +0530

    Adding Landing Page and part of the sign in workflow

---
+++ SplitWiser/LandingPageVC.swift
@@ -0,0 +1,45 @@
+//
+//  LandingPageVC.swift
+//  SplitWiser
+//
+//  Created by Prashanth Moorthy on 26/12/17.
+//  Copyright © 2017 Santebidanur Nagaraja, Pavan Kowshik. All rights reserved.
+//
+
+import UIKit
+
+class LandingPageVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func loginToApp(_ sender: UIButton) {
+
+        self.performSegue(withIdentifier: "showEvents", sender: nil)
+
+        
+//        if Auth.auth().currentUser != nil {
+//            self.navigationBar.isHidden = false
+//            return
+//        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
