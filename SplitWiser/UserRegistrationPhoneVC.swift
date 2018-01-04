f02dfb2f3701bc4c1bb5934780ceb13f6dd8ca04
Prashanth Moorthy <chapparfellow@gmail.com>
2018-01-05 00:44:58 +0530

    Recreating UI for Phone Number add

--- SplitWiser/UserRegistrationPhoneVC.swift
+++ SplitWiser/UserRegistrationPhoneVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class UserRegistrationPhoneVC: UIViewController {
 
@@ -18,14 +19,60 @@ class UserRegistrationPhoneVC: UIViewController {
             self.navigationController?.navigationBar.prefersLargeTitles = true
         }
         self.title = "Your Phone"
+        continueButton.layer.cornerRadius = 10.0
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBOutlet weak var continueButton: UIButton!
+    @IBOutlet weak var countryCode: UITextField!
+    @IBOutlet weak var thePhoneNumber: UITextField!
+    
+    @IBAction func processPhoneNumber(_ sender: UIButton) {
+        
+        self.view.endEditing(true)
+
+        let phoneNumber = "+" + countryCode.text! + thePhoneNumber.text!
+        
+        PhoneAuthProvider.provider().verifyPhoneNumber(phoneNumber, uiDelegate: nil) { (verificationID, error) in
+            if let error = error {
+                //self.showMessagePrompt(error.localizedDescription)
+                print("We have the bloody Error man..... \(error.localizedDescription)")
+                
+                return
+            }
+            // Sign in using the verificationID and the code sent to the user
+            // ...
+            UserDefaults.standard.set(verificationID, forKey: "authVerificationID")
+            
+            self.performSegue(withIdentifier: "showVerificaitonCodeScreen", sender: self)
+        }
+
     }
     
-
+    
+//    @IBAction func signInTheUser(sender : UIButton){
+//        
+//        let verificationID = UserDefaults.standard.string(forKey: "authVerificationID")
+//        //let verificationCode = self.codeTextField.text!
+//        
+//        let credential = PhoneAuthProvider.provider().credential(
+//            withVerificationID: verificationID!,
+//            verificationCode: verificationCode)
+//        
+//        Auth.auth().signIn(with: credential) { (user, error) in
+//            if let error = error {
+//                // ...
+//                return
+//            }
+//            // User is signed in
+//            // ...
+//            print("We have signed in the bugger successfully man!")
+//        }
+//    }
+    
+    
+    
+    
+    
     /*
     // MARK: - Navigation
 
